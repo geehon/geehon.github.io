3a2be46e804bda660d834531861e993bdba0a19c
Geehon <liaoliaoliao70@gmail.com>
2019-01-28 07:33:28 +0800

    Site updated: 2019-01-28 07:33:18

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 27 2019 22:34:39 GMT+0800 (GMT+08:00)
+// build time:Mon Jan 28 2019 07:33:12 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
